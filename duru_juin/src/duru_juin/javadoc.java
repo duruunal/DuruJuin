88e9d816f544c3420869e3857d0ef58bb72ed833
duruunal <duruunal@localhost>
2021-06-09 08:53:06 -0400

    3 Juin, video javadoc

---
+++ duru_juin/src/duru_juin/javadoc.java
@@ -0,0 +1,41 @@
+/*
+* Author: duruunal
+* Date: Jun. 9, 2021
+* Description: le video javadoc
+*/
+
+package duru_juin;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import com.sun.glass.events.KeyEvent;
+
+public class javadoc {
+	
+	
+	
+	/**
+	* initialiser les méthodes définies par le contrôleur
+	*/	
+	
+public void initialize(URL location, ResourceBundle resources) {
+	
+}
+
+
+/**
+* @param e: methode permetant de....
+*/
+@SuppressWarnings("unused")
+private void verifNum(KeyEvent e) {
+	
+}
+/**
+* @param a
+* @param b
+* @param c
+* @param d
+* @param tbl
+*/
+}
